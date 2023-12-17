c5b26aa79c1d7f512fc79226f058755ee26681c6
vanmarkic <drag.markovic@gmail.com>
2023-12-17 18:25:43 +0100

    remove 0 from initial rent field

--- src/components/RentCalculator.tsx
+++ src/components/RentCalculator.tsx
@@ -7,7 +7,142 @@ import {
   energyEfficiencyRatings,
 } from '../formula/rent-increase-formula';
 import styled from 'styled-components';
-import { is } from 'date-fns/locale';
+
+const RentCalculator: React.FC = () => {
+  const [indexationDate, setIndexationDate] = useState<number>(2023);
+  const [initialRent, setInitialRent] = useState<number>(0);
+  const [contractSignatureDate, setContractSignatureDate] = useState<Date>(new Date());
+  const [agreementStartDate, setAgreementStartDate] = useState<Date>(new Date());
+  const [newRent, setNewRent] = useState<number | string>(0);
+  const [region, setRegion] = useState<Regions>('wallonia');
+  const [energyCertificate, setEnergyCertificate] = useState<EnergyEfficiencyRating>('none');
+
+  const isValid =
+    indexationDate &&
+    initialRent &&
+    contractSignatureDate &&
+    agreementStartDate &&
+    indexationDate > agreementStartDate.getFullYear() &&
+    agreementStartDate >= contractSignatureDate;
+
+  const handleCalculate = (e: any) => {
+    e.preventDefault();
+    if (isValid) {
+      try {
+        const increasedRent = calculateRentIndexation(
+          contractSignatureDate,
+          agreementStartDate,
+          initialRent,
+          indexationDate,
+          region,
+          energyCertificate,
+        );
+        if (!increasedRent) {
+          setNewRent('something went wrong');
+          return;
+        }
+        setNewRent(increasedRent);
+      } catch (error: any) {
+        alert(error.message);
+      }
+    }
+  };
+
+  return (
+    <StyledContainer>
+      <h4>Calculateur de loyer</h4>
+      <form>
+        <StyledLabel htmlFor="indexationDate">
+          Année de demande d'indexation:
+          <StyledInput
+            type="number"
+            min="1984"
+            max={new Date().getFullYear()}
+            required
+            lang="fr-FR"
+            id="indexationDate"
+            defaultValue={2023}
+            onChange={(e) => setIndexationDate(Number(e.target.value))}
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="initialRent">
+          Loyer stipulé sur le bail:
+          <StyledInput
+            type="tel"
+            id="initialRent"
+            // defaultValue={initialRent}
+            onChange={(e) => setInitialRent(Number(e.target.value))}
+            required
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="contractSignatureDate">
+          Date de signature du bail:
+          <StyledInput
+            type="date"
+            min="2000-01-01"
+            max={new Date().toISOString().split('T')[0]}
+            required
+            lang="fr-FR"
+            id="contractSignatureDate"
+            onChange={(e) => setContractSignatureDate(new Date(e.target.value))}
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="agreementStartDate">
+          Date d'entrée en vigueur:
+          <StyledInput
+            type="date"
+            min="2000-01-01"
+            max={new Date().toISOString().split('T')[0]}
+            required
+            lang="fr-FR"
+            id="agreementStartDate"
+            onChange={(e) => setAgreementStartDate(new Date(e.target.value))}
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="region">
+          Région:
+          <StyledInput
+            type="select"
+            disabled
+            required
+            lang="fr-FR"
+            id="region"
+            defaultValue="Wallonie"
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="peb">
+          Certificat PEB:
+          <StyledSelect
+            disabled={
+              indexationDate < ENERGY_RATIOS[region].start.getFullYear() ||
+              (indexationDate == ENERGY_RATIOS[region].start.getFullYear() &&
+                agreementStartDate.getMonth() < ENERGY_RATIOS[region].start.getMonth())
+            }
+            name="peb"
+            id="peb"
+            defaultValue={energyCertificate}
+            onChange={(e) => setEnergyCertificate(e.target.value as EnergyEfficiencyRating)}
+          >
+            {energyEfficiencyRatings.map((rating) => (
+              <option value={rating}>{rating !== 'none' ? rating : 'Aucun certificat'}</option>
+            ))}
+          </StyledSelect>
+        </StyledLabel>
+        <StyledContainerRow>
+          <StyledButton disabled={!isValid} type="button" onClick={handleCalculate}>
+            Calculer
+          </StyledButton>
+          <StyledNewRent>
+            <h6>Loyer Indexé</h6>
+            <h4>{Number(newRent).toFixed(2).toLocaleLowerCase('fr-FR')}€</h4>
+          </StyledNewRent>
+        </StyledContainerRow>
+      </form>
+    </StyledContainer>
+  );
+};
+
+export default RentCalculator;
 
 const StyledContainer = styled.div`
   display: flex;
@@ -108,148 +243,10 @@ const StyledButton = styled.button`
   &:active {
     background-color: var(--red);
     outline: 0;
-    border: solid 1px var(--red);
   }
 
   &:hover {
     background-color: var(--red);
     outline: 0;
-    border: solid 1px var(--red);
   }
 `;
-
-const RentCalculator: React.FC = () => {
-  const [indexationDate, setIndexationDate] = useState<number>(2023);
-  const [initialRent, setInitialRent] = useState<number>(0);
-  const [contractSignatureDate, setContractSignatureDate] = useState<Date>(new Date());
-  const [agreementStartDate, setAgreementStartDate] = useState<Date>(new Date());
-  const [newRent, setNewRent] = useState<number | string>(0);
-  const [region, setRegion] = useState<Regions>('wallonia');
-  const [energyCertificate, setEnergyCertificate] = useState<EnergyEfficiencyRating>('none');
-
-  const isValid =
-    indexationDate &&
-    initialRent &&
-    contractSignatureDate &&
-    agreementStartDate &&
-    indexationDate > agreementStartDate.getFullYear() &&
-    agreementStartDate >= contractSignatureDate;
-
-  const handleCalculate = (e: any) => {
-    e.preventDefault();
-    if (isValid) {
-      try {
-        const increasedRent = calculateRentIndexation(
-          contractSignatureDate,
-          agreementStartDate,
-          initialRent,
-          indexationDate,
-          region,
-          energyCertificate,
-        );
-        if (!increasedRent) {
-          setNewRent('something went wrong');
-          return;
-        }
-        setNewRent(increasedRent);
-      } catch (error: any) {
-        alert(error.message);
-      }
-    }
-  };
-
-  return (
-    <StyledContainer>
-      <h4>Calculateur de loyer</h4>
-      <form>
-        <StyledLabel htmlFor="indexationDate">
-          Année de demande d'indexation:
-          <StyledInput
-            type="number"
-            min="1984"
-            max={new Date().getFullYear()}
-            required
-            lang="fr-FR"
-            id="indexationDate"
-            defaultValue={2023}
-            onChange={(e) => setIndexationDate(Number(e.target.value))}
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="initialRent">
-          Loyer stipulé sur le bail:
-          <StyledInput
-            type="tel"
-            id="initialRent"
-            defaultValue={initialRent}
-            onChange={(e) => setInitialRent(Number(e.target.value))}
-            required
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="contractSignatureDate">
-          Date de signature du bail:
-          <StyledInput
-            type="date"
-            min="2000-01-01"
-            max={new Date().toISOString().split('T')[0]}
-            required
-            lang="fr-FR"
-            id="contractSignatureDate"
-            onChange={(e) => setContractSignatureDate(new Date(e.target.value))}
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="agreementStartDate">
-          Date d'entrée en vigueur:
-          <StyledInput
-            type="date"
-            min="2000-01-01"
-            max={new Date().toISOString().split('T')[0]}
-            required
-            lang="fr-FR"
-            id="agreementStartDate"
-            onChange={(e) => setAgreementStartDate(new Date(e.target.value))}
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="region">
-          Région:
-          <StyledInput
-            type="select"
-            disabled
-            required
-            lang="fr-FR"
-            id="region"
-            defaultValue="Wallonie"
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="peb">
-          Certificat PEB:
-          <StyledSelect
-            disabled={
-              indexationDate < ENERGY_RATIOS[region].start.getFullYear() ||
-              (indexationDate == ENERGY_RATIOS[region].start.getFullYear() &&
-                agreementStartDate.getMonth() < ENERGY_RATIOS[region].start.getMonth())
-            }
-            name="peb"
-            id="peb"
-            defaultValue={energyCertificate}
-            onChange={(e) => setEnergyCertificate(e.target.value as EnergyEfficiencyRating)}
-          >
-            {energyEfficiencyRatings.map((rating) => (
-              <option value={rating}>{rating !== 'none' ? rating : 'Aucun certificat'}</option>
-            ))}
-          </StyledSelect>
-        </StyledLabel>
-        <StyledContainerRow>
-          <StyledButton disabled={!isValid} type="button" onClick={handleCalculate}>
-            Calculer
-          </StyledButton>
-          <StyledNewRent>
-            <h6>Loyer Indexé</h6>
-            <h4>{Number(newRent).toFixed(2).toLocaleLowerCase('fr-FR')}€</h4>
-          </StyledNewRent>
-        </StyledContainerRow>
-      </form>
-    </StyledContainer>
-  );
-};
-
-export default RentCalculator;
