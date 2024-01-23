653e0b3ca7490f2a85abe5b536350eee1bb9404f
vanmarkic <drag.markovic@gmail.com>
2024-01-23 21:25:52 +0100

    refactor ToggleButton with Radix component

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,9 +1,11 @@
-import React , {useState }from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 import { Regions } from '../formula/types-and-constants';
+import * as Switch from '@radix-ui/react-switch';
+import './ToggleButton.css';
 
 interface ToggleButtonProps {
-  onClick: (region: Regions)  => void; // Définissez le type de la fonction onClick
+  onClick: (region: Regions) => void;
 }
 
 const Wrapper = styled.div`
@@ -13,70 +15,68 @@ const Wrapper = styled.div`
   position: relative;
 `;
 
-export const Button = styled.div<{ active: boolean }>`
+export const Button = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 80px;
-  height:30px;
+  height: 30px;
   border: none;
   border-radius: 30px;
-  background-color:var(--red);
-  box-shadow:inset 1px 1px 1px black;
+  background-color: var(--red);
+  box-shadow: inset 1px 1px 1px black;
 `;
-
 
 export const ButtonText = styled.button<{ active: boolean }>`
   text-align: left;
   z-index: 1000;
-  border:none;
-  box-shadow:none;
-  color: ${(props) => (props.active ? "var(--red)" : 'var(--blue)')};
-  background-color: white;
-  font-size:20px;
+  border: none;
+  box-shadow: none;
+  color: ${(props) => (props.active ? 'var(--red)' : 'var(--blue)')};
+  background-color: transparent;
+  font-size: 20px;
+  margin: 0.5rem;
 `;
 
 export const Circle = styled.button<{ active: boolean }>`
   width: 30px;
   height: 30px;
   border-radius: 50%;
-  border:none;
+  border: none;
   text-align: right;
   z-index: 1000;
-  box-shadow: ${(props) => (props.active ? "2px 1px 1px black" : 'none')};
+  box-shadow: ${(props) => (props.active ? '2px 1px 1px black' : 'none')};
   background-color: ${(props) => (props.active ? 'white' : 'transparent')};
 `;
 
+const ToggleButton: React.FC<ToggleButtonProps> = ({ onClick }) => {
+  const [activeRegion, setActiveRegion] = useState<Regions>('brussels');
 
-
-const ToggleButton: React.FC<ToggleButtonProps> = ({ onClick,  }) => {
-
-  const [activeRegion, setActiveRegion] = useState("brussels");
-
-  const handleToggle = (region: Regions)  => {
+  const handleToggle = (region: Regions) => {
     setActiveRegion(region);
     onClick(region);
   };
 
   return (
     <Wrapper>
-      <ButtonText
-        active={activeRegion === "brussels"}
-        onClick={() => handleToggle("brussels")}
-      >
-        BRUXELLES
-      </ButtonText>
-      <Button active={activeRegion === "wallonia"}>
-      <Circle active={activeRegion === "brussels"}/>
-        <Circle active={activeRegion === "wallonia"}/>
-      </Button>
-     
-      <ButtonText
-        active={activeRegion === "wallonia"}
-        onClick={() => handleToggle("wallonia")}
-      >
-        WALLONIE
-      </ButtonText>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <ButtonText active={activeRegion === 'brussels'} onClick={() => handleToggle('brussels')}>
+          BRUXELLES
+        </ButtonText>
+
+        <Switch.Root
+          checked={activeRegion === 'wallonia'}
+          className="SwitchRoot"
+          id="airplane-mode"
+          onCheckedChange={(e) => handleToggle(e ? 'wallonia' : 'brussels')}
+        >
+          <Switch.Thumb className="SwitchThumb" />
+        </Switch.Root>
+
+        <ButtonText active={activeRegion === 'wallonia'} onClick={() => handleToggle('wallonia')}>
+          WALLONIE
+        </ButtonText>
+      </div>
     </Wrapper>
   );
 };
