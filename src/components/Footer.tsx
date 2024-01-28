19af5545ccb314bb3f87ceec702c6a24eace36e2
aurorevd <aurorevandamme@gmail.com>
2024-01-28 16:20:38 +0100

    add links ang style on toggle button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,27 +11,77 @@ const StyledFooter = styled.footer`
   gap: 20px;
   margin-top: 60px;
 `;
+const StyledA=styled.a`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
 export const Footer: React.FC<any> = () => {
+
   return (
     <StyledFooter>
-      <a href="https://rwdh.be/" target="_blank">
+      <StyledA href="https://rwdh.be/" target="_blank">
         <StaticImage
           placeholder="none"
-          height={40}
+          height={30}
           alt="rwdh logo"
           src="../logo/partners/rwdh.svg"
         />
-      </a>
-      <StaticImage placeholder="none" height={40} alt="csc logo" src="../logo/partners/csc.png" />
-      <StaticImage placeholder="none" height={40} alt="csc logo" src="../logo/partners/moc.png" />
-      <StaticImage placeholder="none" height={40} alt="csc logo" src="../logo/partners/rapel.png" />
-      <StaticImage
-        placeholder="none"
-        height={40}
-        alt="csc logo"
-        src="../logo/partners/solidaris.png"
-      />
+      </StyledA>
+
+      <StyledA href="https://www.lacsc.be/" target="_blank">
+        <StaticImage placeholder="none" height={40} alt="csc logo" src="../logo/partners/csc.png" />
+      </StyledA>
+
+      <StyledA href="https://www.moc.be/" target="_blank">
+        <StaticImage placeholder="none" height={40} alt="moc logo" src="../logo/partners/moc.png" />
+      </StyledA>
+
+      <StyledA href="https://rapel.be/" target="_blank">
+        <StaticImage
+          placeholder="none"
+          height={40}
+          alt="rapel logo"
+          src="../logo/partners/rapel.png"
+        />
+      </StyledA>
+
+      <StyledA href="https://www.solidaris.be/" target="_blank">
+        <StaticImage
+          placeholder="none"
+          height={30}
+          alt="csc logo"
+          src="../logo/partners/solidaris.png"
+        />
+      </StyledA>
+
+      <StyledA href="https://fgtb-wallonne.be/" target="_blank">
+        <StaticImage
+          placeholder="none"
+          height={35}
+          alt="csc logo"
+          src="../logo/partners/FGTB.jpg"
+        />
+      </StyledA>
+
+      <StyledA href="https://www.rwade.be/" target="_blank">
+        <StaticImage
+          placeholder="none"
+          height={50}
+          alt="csc logo"
+          src="../logo/partners/RWADE.jpg"
+        />
+      </StyledA>
+
+      <StyledA href="https://www.cepag.be/" target="_blank">
+        <StaticImage
+          placeholder="none"
+          height={40}
+          alt="csc logo"
+          src="../logo/partners/CEPAG.jpg"
+        />
+      </StyledA>
     </StyledFooter>
   );
 };
