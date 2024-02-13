5efeff0428bfe2bcc2460bb92e42bbb211de9d98
aurorevd <aurorevandamme@gmail.com>
2024-02-13 10:39:26 +0100

    change style and div & add text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,99 +5,122 @@ import styled, { css } from 'styled-components';
 const StyledFooter = styled.footer`
   width: 100%;
   display: flex;
-  padding: 2rem;
+  padding:  0.5rem 1rem;
   justify-content: space-around;
   flex-wrap: wrap;
   gap: 20px;
-  margin-top: 60px;
 `;
-const StyledA=styled.a`
-display: flex;
-justify-content: center;
-align-items: center;
+
+
+const StyledContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const StyledA = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Footer: React.FC<any> = () => {
-
   return (
     <StyledFooter>
-      <StyledA href="https://rwdh.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={30}
-          alt="rwdh logo"
-          src="../logo/partners/rwdh.svg"
-        />
-      </StyledA>
+      <h6>UNE INITIATIVE DE :</h6>
+      <StyledContainer>
+        <StyledA href="https://rwdh.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={60}
+            alt="rwdh logo"
+            src="../logo/partners/rwdh.svg"
+          />
+        </StyledA>
+      </StyledContainer>
+      <h6>AVEC LE SOUTIEN DE :</h6>
+      <StyledContainer>
+        <StyledA href="https://www.lacsc.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={40}
+            alt="csc logo"
+            src="../logo/partners/csc.png"
+          />
+        </StyledA>
 
-      <StyledA href="https://www.lacsc.be/" target="_blank">
-        <StaticImage placeholder="none" height={40} alt="csc logo" src="../logo/partners/csc.png" />
-      </StyledA>
+        <StyledA href="https://www.moc.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={40}
+            alt="moc logo"
+            src="../logo/partners/moc.png"
+          />
+        </StyledA>
 
-      <StyledA href="https://www.moc.be/" target="_blank">
-        <StaticImage placeholder="none" height={40} alt="moc logo" src="../logo/partners/moc.png" />
-      </StyledA>
+        <StyledA href="https://rapel.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={40}
+            alt="rapel logo"
+            src="../logo/partners/rapel.png"
+          />
+        </StyledA>
 
-      <StyledA href="https://rapel.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={40}
-          alt="rapel logo"
-          src="../logo/partners/rapel.png"
-        />
-      </StyledA>
+        <StyledA href="https://www.solidaris.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={30}
+            alt="csc logo"
+            src="../logo/partners/solidaris.png"
+          />
+        </StyledA>
 
-      <StyledA href="https://www.solidaris.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={30}
-          alt="csc logo"
-          src="../logo/partners/solidaris.png"
-        />
-      </StyledA>
+        <StyledA href="https://fgtb-wallonne.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={35}
+            alt="csc logo"
+            src="../logo/partners/FGTB.jpg"
+          />
+        </StyledA>
 
-      <StyledA href="https://fgtb-wallonne.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={35}
-          alt="csc logo"
-          src="../logo/partners/FGTB.jpg"
-        />
-      </StyledA>
+        <StyledA href="https://www.rwade.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={50}
+            alt="csc logo"
+            src="../logo/partners/RWADE.jpg"
+          />
+        </StyledA>
 
-      <StyledA href="https://www.rwade.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={50}
-          alt="csc logo"
-          src="../logo/partners/RWADE.jpg"
-        />
-      </StyledA>
-
-      <StyledA href="https://www.cepag.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={40}
-          alt="csc logo"
-          src="../logo/partners/CEPAG.jpg"
-        />
-      </StyledA>
-      <StyledA href="https://www.rwlp.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={40}
-          alt="csc logo"
-          src="../logo/partners/Rwlp.png"
-        />
-      </StyledA>
-      <StyledA href="https://www.equipespopulaires.be/" target="_blank">
-        <StaticImage
-          placeholder="none"
-          height={40}
-          alt="csc logo"
-          src="../logo/partners/EP.png"
-        />
-      </StyledA>
+        <StyledA href="https://www.cepag.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={40}
+            alt="csc logo"
+            src="../logo/partners/CEPAG.jpg"
+          />
+        </StyledA>
+        <StyledA href="https://www.rwlp.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={30}
+            alt="csc logo"
+            src="../logo/partners/Rwlp.png"
+          />
+        </StyledA>
+        <StyledA href="https://www.equipespopulaires.be/" target="_blank">
+          <StaticImage
+            placeholder="none"
+            height={40}
+            alt="csc logo"
+            src="../logo/partners/EP.png"
+          />
+        </StyledA>
+      </StyledContainer>
     </StyledFooter>
   );
 };
